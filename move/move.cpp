e41f6808c760a62a9d310062012c964369287218
Dmitry Volkov <lorddmitry22@gmail.com>
2024-07-29 16:41:41 +0300

    update

---
+++ move/move.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class string{
+public:
+    string(string&& other) 
+    : arr(other.arr), sz(other.sz), cap(other.cap) {
+        other.arr = nullptr;
+        other.sz = other.cap = 0;
+    }
+    
+    string& operator=(string&& other) {
+        delete[] arr;
+        
+        arr = other.arr; other.arr = nullptr;
+        sz = other.sz; other.sz = 0;
+        cap = other.cap; other.cap = 0;
+        return *this;
+    }
+
+private:
+    char* arr;
+    size_t sz;
+    size_t cap;
+};
+
+int main(){
+}
